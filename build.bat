1b2d34fb6e2059a89499f2997d3b516ad6baf907
BitByBit <BitByBitW@outlook.com>
2026-08-31 09:58:16 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,3 @@
+g++ -std=c++20 -O2 %~dp0src\compile.cpp -o %~dp0bin\compile.exe
+g++ -std=c++20 -O2 %~dp0src\expander.cpp -o %~dp0bin\expander.exe
+g++ -std=c++20 -O2 %~dp0src\run.cpp -o %~dp0bin\run.exe
